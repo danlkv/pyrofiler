55147160567b7f752774f31ffd44323634e8c65d
Danil Lykov <lkv97dn@gmail.com>
2020-03-04 14:14:40 -0600

    add tests for simple usage

--- tests/test_pyrofiler.py
+++ tests/test_pyrofiler.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python
 
 """Tests for `pyrofiler` package."""
+import sys
+from math import sin, cos, radians
+import time
 
-import pytest
-
-
-from pyrofiler import pyrofiler
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from pyrofiler.pyrofiler import Profiler
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_time(capsys):
+    descr = 'test descr'
+    sleep_time = .3
+    p = Profiler()
+
+    # -- actual test
+    @p.timing(descr)
+    def sleep_x(x):
+        time.sleep(x)
+    sleep_x(sleep_time)
+    # --
+
+    out, err = capsys.readouterr()
+    assert descr in out
+    # Last number is the value
+    assert abs(sleep_time - float(out.split()[-1])) <.001
+
+def test_cpu(capsys):
+    descr = 'test descr cpu'
+    p = Profiler()
+
+    # -- actual test
+    @p.timing(descr)
+    def bench():
+        """ Multiplies 1 by sin^2 + cos^2 for all angles """
+        product = 1.0
+        for counter in range(1, 1000, 1):
+            for dex in list(range(1, 360, 1)):
+                angle = radians(dex)
+                product *= sin(angle)**2 + cos(angle)**2
+        return product
+    x = bench()
+    assert abs(x-1)<.0001
+    # --
+
+    out, err = capsys.readouterr()
+    assert descr in out
+    # Last number is the value
+    assert float(out.split()[-1]) > .001
+
