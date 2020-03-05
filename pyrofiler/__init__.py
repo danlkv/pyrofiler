796ef72079e98e830818d6894233f09cec3afdae
Danil Lykov <lkv97dn@gmail.com>
2020-03-04 19:16:39 -0600

    export profiling decorators

--- pyrofiler/__init__.py
+++ pyrofiler/__init__.py
@@ -2,6 +2,10 @@
 
 __author__ = """Dan Lykov"""
 __email__ = 'lkv97dn@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from pyrofiler.threaded import threaded
+from pyrofiler.profilers import (
+    timing, timed
+    , cpu_util, mem_util, proc_count
+)
