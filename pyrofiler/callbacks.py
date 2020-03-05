7a372e9c6d40f77138afe9f838399d755f1a9ae6
Danil Lykov <lkv97dn@gmail.com>
2020-03-04 19:51:05 -0600

    added callbacks bank

---
+++ pyrofiler/callbacks.py
@@ -0,0 +1,18 @@
+
+def append(data, value, **kwargs):
+    key = kwargs['description']
+    data[key].append(value)
+
+def assign(data, value, **kwargs):
+    key = kwargs['description']
+    data[key] = value
+
+def append_to(data):
+    def _callback(value, **kwargs):
+        append(data, value, **kwargs)
+    return _callback
+
+def assign_to(data):
+    def _callback(value, **kwargs):
+        assign(data, value, **kwargs)
+    return _callback
