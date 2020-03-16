91254b01da69b9963a4aa732075500a3a63170c7
Danil Lykov <lkv97dn@gmail.com>
2020-03-16 12:51:25 -0500

    update deps

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 'psutil', 'numpy']
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['pytest>=3', 'numpy']
 
 setup(
     author="Dan Lykov",
