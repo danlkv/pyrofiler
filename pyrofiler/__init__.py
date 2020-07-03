75c9f8340d52a5b1b4f3a862b0a88c76f9e2aea8
Danil Lykov <lkv97dn@gmail.com>
2020-07-03 09:20:48 -0500

    bump to 0.1.4

--- pyrofiler/__init__.py
+++ pyrofiler/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dan Lykov"""
 __email__ = 'lkv97dn@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from pyrofiler.threaded import threaded
 from pyrofiler.pyrofiler import Profiler
