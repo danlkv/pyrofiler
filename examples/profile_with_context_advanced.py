6449a8df2c0f93a3827520a31b136063c1e18ce9
Danil Lykov <lkv97dn@gmail.com>
2022-02-11 17:39:52 -0600

    add advanced pyrofiler example

---
+++ examples/profile_with_context_advanced.py
@@ -0,0 +1,35 @@
+from pyrofiler import Profiler
+import numpy as np
+import time
+
+class MyProfiler(Profiler):
+    def __init__(self, callback=None):
+        super().__init__(callback=callback)
+        self.use_append()
+
+    def get_stats(self, label):
+        data = self.data[label]
+        return dict(
+            mean=np.mean(data),
+            max = np.max(data),
+            std = np.std(data),
+            min = np.min(data)
+        )
+
+prof = MyProfiler()
+
+def my_function(i):
+    with prof.timing('My_func_time'):
+        time.sleep(i)
+
+def main():
+    with prof.timing('Main'):
+        for i in range(10):
+            my_function(i/20)
+
+main()
+print('Pyrofiler data', prof.data)
+print('Pyrofiler main', prof.data['Main'])
+print('Pyrofiler my_function sum', sum(prof.data['My_func_time']))
+print('Overhead of python/pyrofiler', prof.data['Main'][0] - sum(prof.data['My_func_time']))
+print('Stats', prof.get_stats('My_func_time'))
